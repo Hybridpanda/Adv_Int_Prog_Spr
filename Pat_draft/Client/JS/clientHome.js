9e31de3f173ab2c919e381a8b7f889853c2a19d9
PatrickCasey <pcasey10101@gmail.com>
2020-09-17 21:51:25 +1000

    created a simple start of the front end to implement a favor system

--- Pat_draft/Client/JS/clientHome.js
+++ Pat_draft/Client/JS/clientHome.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+//console.log('Hello');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const recipient = formData.get('recipient');
+    const recipient = formData.get('recipient');
+
+    const serv = {
+        name,
+        content
+    };
+    console.log(serv);
+    console.log('form was submitted');
+
+});
