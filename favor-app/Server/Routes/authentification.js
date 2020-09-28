52992eccab0c40afe66b7812fa9e42ff863e6c25
PatrickCasey <pcasey10101@gmail.com>
2020-09-28 15:14:27 +1000

    abe to create and save a new user

--- favor-app/Server/Routes/authentification.js
+++ favor-app/Server/Routes/authentification.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const pool = require("../db");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
   try {
@@ -12,8 +13,22 @@ router.post("/register", async (req, res) => {
     );
 
     if (user.rows.length !== 0) {
-      return res.status(401).send("user already exists");
+      return res.status(401).json("user already exists");
     }
+    //3. bcrypt the user password
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+    //4. enter the new user in the database
+    const newUser = await pool.query(
+      "INSERT INTO authusers (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
+
+    res.json(newUser.rows[0]);
+
+    //5. generate jwt token
   } catch (err) {
     console.error(err.message);
     res.status(500).json("Server Error");
