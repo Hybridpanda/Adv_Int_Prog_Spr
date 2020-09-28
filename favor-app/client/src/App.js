25aa816b70bf0c6939ddb0a8c346530030110f10
PatrickCasey <pcasey10101@gmail.com>
2020-09-28 16:13:35 +1000

    created some fragment for the titles

--- favor-app/client/src/App.js
+++ favor-app/client/src/App.js
@@ -1,5 +1,12 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return <Fragment></Fragment>;
---
+++ favor-app/client/src/components/profile.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Profile = () => {
+  return (
+    <Fragment>
+      <h1>Profile</h1>
+    </Fragment>
+  );
+};
+
+export default Profile;
