63906641342a08ae94a4dbac98cd9bf2b2a867e6
PatrickCasey <pcasey10101@gmail.com>
2020-09-28 17:14:16 +1000

    register now works
    the register can now send form data to the server and the server can send back the token and the client changes screen

--- favor-app/client/src/components/register.js
+++ favor-app/client/src/components/register.js
@@ -1,9 +1,67 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
-const Register = () => {
+const Register = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const { email, password, name } = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formBody = { email, password, name };
+      const response = await fetch("http://localhost:5000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formBody),
+      });
+
+      const parseRes = await response.json();
+      localStorage.setItem("token", parseRes.token);
+
+      setAuth(true);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <Fragment>
-      <h1>Register</h1>
+      <h1 className="text-center my-5">Register</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          className="form-control my-3"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          className="form-control my-3"
+          value={password}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="name"
+          name="name"
+          placeholder="name"
+          className="form-control my-3"
+          value={name}
+          onChange={(e) => onChange(e)}
+        />
+        <button className="btn btn-success btn-block">submit</button>
+      </form>
     </Fragment>
   );
 };
