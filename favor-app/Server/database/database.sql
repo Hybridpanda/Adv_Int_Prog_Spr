3fbcb46285be45645f8d185ba9c5391e61630d94
PatrickCasey <pcasey10101@gmail.com>
2020-10-02 19:46:52 +1000

    tried to create a new scheme for favors

--- favor-app/Server/database/database.sql
+++ favor-app/Server/database/database.sql
@@ -3,11 +3,20 @@ CREATE DATABASE favorapp;
 -- set extension https://www.xtuple.com/knowledge/how-do-i-install-uuid-ossp
 -- create extension if not exists "uuid-ossp";
 CREATE TABLE authusers(
-    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid //PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
+    user_password VARCHAR(255) NOT NULL,
+    //PRIMARY KEY(user_id)
 );
 
 -- fake data the values need to be in single quotes
 INSERT INTO authusers (user_name, user_email, user_password) VALUES ('henry', 'henry123@gmail.com', 'cat123456');
+
+CREATE TABLE favors(
+    favor_id SERIAL,
+    user_id UUID,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (favor_id),
+    FOREIGN KEY (user_id) REFERENCES authusers(user_id)
+)
