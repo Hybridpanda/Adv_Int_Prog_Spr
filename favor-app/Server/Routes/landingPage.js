4c03262405eaf1c0ce966364fb5a8cf5c527d63d
Hybridpanda <41685376+Hybridpanda@users.noreply.github.com>
2020-10-20 13:10:47 +1100

    added some functionality to the site

--- favor-app/Server/Routes/landingPage.js
+++ favor-app/Server/Routes/landingPage.js
@@ -1,7 +1,18 @@
+const pool = require("../db");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.json("LANDING");
+  try {
+    
+
+    const board = await pool.query(
+      "SELECT user_name, user_email FROM authusers"
+    );
+    res.json(board.rows);
+
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 module.exports = router;
